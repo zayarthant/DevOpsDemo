cd1553b250f592a50e9cbca0e46e2a009b561134
zayarthant.deca@gmail.com <zayarthant.deca@gmail.com>
2020-08-08 01:16:06 +0630

    update DockerFile

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.validation.constraints.AssertTrue;
+
 @SpringBootTest
 class DemoApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
